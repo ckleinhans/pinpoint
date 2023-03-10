b24c2262b72e4c2ff075b49ba43fcae19c1cc2ea
Mark <mxj220@gmail.com>
2023-03-10 16:56:28 -0600

    navbar ui created
    Navbar UI complete, no color theming or functionality

--- app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import edu.wisc.ece.pinpoint.utils.FirebaseDriver;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDriver firebase;
+    private BottomNavigationView navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebase = FirebaseDriver.getInstance();
+        navBar = findViewById(R.id.navBar);
+        navBar.getMenu().getItem(2).setEnabled(false);
     }
 
     @Override
