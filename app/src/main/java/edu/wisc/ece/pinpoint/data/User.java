85e3a472c25f74a586d57c2f1147511353faf58c
Caelan Kleinhans <55991831+ckleinhans@users.noreply.github.com>
2023-03-19 17:39:52 -0500

    Create user data model

---
+++ app/src/main/java/edu/wisc/ece/pinpoint/data/User.java
@@ -0,0 +1,83 @@
+package edu.wisc.ece.pinpoint.data;
+
+public class User {
+    private String username;
+    private String bio;
+    private String location;
+    private String profilePicUrl;
+    private int numFollowers;
+    private int numFollowing;
+    private int numPinsDropped;
+    private int numPinsFound;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getProfilePicUrl() {
+        return profilePicUrl;
+    }
+
+    public void setProfilePicUrl(String profilePicUrl) {
+        this.profilePicUrl = profilePicUrl;
+    }
+
+    public int getNumFollowers() {
+        return numFollowers;
+    }
+
+    public void setNumFollowers(int numFollowers) {
+        this.numFollowers = numFollowers;
+    }
+
+    public int getNumFollowing() {
+        return numFollowing;
+    }
+
+    public void setNumFollowing(int numFollowing) {
+        this.numFollowing = numFollowing;
+    }
+
+    public int getNumPinsDropped() {
+        return numPinsDropped;
+    }
+
+    public void setNumPinsDropped(int numPinsDropped) {
+        this.numPinsDropped = numPinsDropped;
+    }
+
+    public int getNumPinsFound() {
+        return numPinsFound;
+    }
+
+    public void setNumPinsFound(int numPinsFound) {
+        this.numPinsFound = numPinsFound;
+    }
+}
--- app/src/main/java/edu/wisc/ece/pinpoint/utils/FirebaseDriver.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/utils/FirebaseDriver.java
@@ -7,12 +7,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import javax.annotation.Nullable;
+
+import edu.wisc.ece.pinpoint.data.User;
 
 public final class FirebaseDriver {
     private static FirebaseDriver instance;
     private final FirebaseAuth auth;
+    private final FirebaseFirestore db;
 
     private FirebaseDriver() {
         if (instance != null) {
@@ -20,6 +24,7 @@ public final class FirebaseDriver {
         }
         instance = this;
         auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     public static FirebaseDriver getInstance() {
@@ -64,5 +69,10 @@ public final class FirebaseDriver {
 
     public Task<Void> logout(Activity activity) {
         return AuthUI.getInstance().signOut(activity);
+    }
+
+    public Task<User> fetchUser(String uid) {
+        return db.collection("users").document(uid).get()
+                .continueWith(task -> task.getResult().toObject(User.class));
     }
 }
