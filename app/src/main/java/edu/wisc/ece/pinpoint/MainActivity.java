c75ae3540854313d651d1eb76bec61e46ed02abe
Benjamin Petersen <petebs10@gmail.com>
2023-03-19 21:44:42 -0500

    use built in theme support, add map button light mode switch

--- app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
@@ -1,5 +1,5 @@
 package edu.wisc.ece.pinpoint;
-
+import androidx.appcompat.app.AppCompatDelegate;
 import android.os.Bundle;
 import android.view.View;
 
@@ -29,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onMapButtonClick(View view) {
         navController.navigate(R.id.navbarMap);
+
+        //TODO: temporary use map button to switch to light mode, remove later
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         // TODO: temporary use map button to log user out, remove later
         firebase.logout(MainActivity.this);
     }
