bf5e90ec37f7d09b189bb2f395e7a2bcf2119ed8
Mark <mxj220@gmail.com>
2023-03-21 23:28:38 -0500

    Upload and view profile picture

--- app/src/main/java/edu/wisc/ece/pinpoint/pages/profile/EditProfileFragment.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/pages/profile/EditProfileFragment.java
@@ -1,6 +1,10 @@
 package edu.wisc.ece.pinpoint.pages.profile;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,13 +13,19 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import edu.wisc.ece.pinpoint.R;
 import edu.wisc.ece.pinpoint.data.User;
@@ -31,11 +41,40 @@ public class EditProfileFragment extends Fragment {
     private EditText locationInput;
     private EditText bioInput;
     private ImageView profilePicUpload;
+    private ActivityResultLauncher<Intent> launcher;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         firebase = FirebaseDriver.getInstance();
+        // create the launcher that will obtain and upload image
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if(result.getResultCode() == Activity.RESULT_OK && result.getData() != null){
+                Uri photo = result.getData().getData();
+                // base storage reference created
+                StorageReference baseRef = FirebaseStorage.getInstance().getReference();
+                String uid = firebase.getCurrentUser().getUid();
+                User cachedUser = firebase.getCachedUser(uid);
+                // profile image reference
+                StorageReference pictureRef = baseRef.child("users/"+uid);
+                // upload photo
+                UploadTask uploadTask = pictureRef.putFile(photo);
+                uploadTask.addOnFailureListener((exception) -> {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(requireActivity(),"Image upload failed.", Toast.LENGTH_SHORT).show();
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pictureRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                updatePicture(uri, cachedUser, uid);
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -62,6 +101,7 @@ public class EditProfileFragment extends Fragment {
         cancelButton.setOnClickListener(
                 (buttonView) -> navController.navigate(EditProfileFragmentDirections.profile()));
         saveButton.setOnClickListener(this::save);
+        profilePicUpload.setOnClickListener(this::uploadPicture);
         User cachedUser = firebase.getCachedUser(firebase.getCurrentUser().getUid());
         if (cachedUser != null) {
             usernameInput.setText(cachedUser.getUsername());
@@ -70,6 +110,25 @@ public class EditProfileFragment extends Fragment {
             // TODO: don't think we need shaped image view since Glide can circle crop image for us
             cachedUser.loadProfilePic(profilePicUpload, this);
         }
+    }
+
+
+
+    private void uploadPicture(View view) {
+        // create gallery-opening intent
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // launch intent
+        launcher.launch(intent);
+
+    }
+
+    private void updatePicture(Uri uri, User cachedUser, String uid) {
+        // Method must exist to execute actions without timing out
+        Toast.makeText(requireActivity(),uri.toString(),Toast.LENGTH_LONG).show();
+        cachedUser.setProfilePicUrl(uri.toString());
+        cachedUser.save(uid);
+        Toast.makeText(requireActivity(),"Image successfully uploaded.", Toast.LENGTH_SHORT).show();
+        navController.navigate(EditProfileFragmentDirections.profile());
     }
 
     private void save(View buttonView) {
