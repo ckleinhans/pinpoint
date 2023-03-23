211faf4a5b04eb3db98eba68f202e0824c2efe20
Sriram Alla <breddy@wisc.edu>
2023-03-23 10:54:46 -0500

    grid recycler view added

---
+++ app/src/main/java/edu/wisc/ece/pinpoint/pages/pinList/RecyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package edu.wisc.ece.pinpoint.pages.pinList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+import edu.wisc.ece.pinpoint.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+
+    private ArrayList<RecyclerData> courseDataArrayList;
+
+
+    public RecyclerViewAdapter(ArrayList<RecyclerData> recyclerDataArrayList) {
+        this.courseDataArrayList = recyclerDataArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate Layout
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view, parent, false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+        // Set the data to textview and imageview.
+        RecyclerData recyclerData = courseDataArrayList.get(position);
+        holder.courseTV.setText(recyclerData.getTitle());
+        holder.courseIV.setImageResource(recyclerData.getImgid());
+    }
+
+    @Override
+    public int getItemCount() {
+        // this method returns the size of recyclerview
+        return courseDataArrayList.size();
+    }
+
+    // View Holder Class to handle Recycler View.
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView courseTV;
+        private ImageView courseIV;
+
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            courseTV = itemView.findViewById(R.id.idTVCourse);
+            courseIV = itemView.findViewById(R.id.idIVcourseIV);
+        }
+    }
+}
--- app/src/main/java/edu/wisc/ece/pinpoint/pages/pinList/pinListFragment.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/pages/pinList/pinListFragment.java
@@ -1,52 +1,122 @@
 package edu.wisc.ece.pinpoint.pages.pinList;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//import androidx.navigation.NavController;
+//import androidx.navigation.NavDirections;
+//import androidx.navigation.Navigation;
+//
+//import android.content.Intent;
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageButton;
+//
+//import edu.wisc.ece.pinpoint.R;
+//
+//public class pinListFragment extends Fragment {
+//    ImageButton button1;
+//    NavController navController;
+//
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_pin_list, container, false);
+//    }
+//
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        if (Build.VERSION.SDK_INT > 25) {
+//            button1 = requireView().findViewById(R.id.imageButton);
+//            button1.setTooltipText("PIN 1 with details");
+//            navController = Navigation.findNavController(view);
+//            button1.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    openPinView();
+//                }
+//            });
+//
+//        }
+//    }
+//
+//    public void openPinView(){
+//      NavDirections directions =  pinListFragmentDirections.actionNavbarSearchToPinViewPage();
+//        navController.navigate(directions);
+//
+//    }
+//}
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
 
 import edu.wisc.ece.pinpoint.R;
 
 public class pinListFragment extends Fragment {
-    ImageButton button1;
-    NavController navController;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
+
+    private ArrayList<RecyclerData> recyclerDataArrayList;
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pin_list, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_pin_list, container, false);
+
+
+            recyclerDataArrayList=new ArrayList<>();
+
+            // added data to array list
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DSA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("DA",R.drawable.oldnote));
+            recyclerDataArrayList.add(new RecyclerData("D",R.drawable.oldnote));
+
+
+            GridLayoutManager layoutManager=new GridLayoutManager(getContext(),3);
+
+
+            recyclerView = (RecyclerView) rootView.findViewById(R.id.idCourseRV);
+            adapter = new RecyclerViewAdapter(recyclerDataArrayList);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        if (Build.VERSION.SDK_INT > 25) {
-            button1 = requireView().findViewById(R.id.imageButton);
-            button1.setTooltipText("PIN 1 with details");
-            navController = Navigation.findNavController(view);
-            button1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    openPinView();
-                }
-            });
-
-        }
-    }
-
-    public void openPinView(){
-      NavDirections directions =  pinListFragmentDirections.actionNavbarSearchToPinViewPage();
-        navController.navigate(directions);
 
     }
-}
