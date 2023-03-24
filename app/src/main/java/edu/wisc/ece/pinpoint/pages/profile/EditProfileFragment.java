9e31380d1ea8fb9e344fce07fadd29d1f888b049
Caelan Kleinhans <55991831+ckleinhans@users.noreply.github.com>
2023-03-24 13:38:27 -0500

    [PIN-75] Add user profile location autocomplete (#16)
    * Make nav IDs follow ID naming convention
    
    * Add location autocomplete
    
    * Add API key as secret
    
    * Remove location zipcode & improve functionality
    
    * Fix bug

--- app/src/main/java/edu/wisc/ece/pinpoint/pages/profile/EditProfileFragment.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/pages/profile/EditProfileFragment.java
@@ -1,6 +1,8 @@
 package edu.wisc.ece.pinpoint.pages.profile;
 
-import android.app.Activity;
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -23,39 +25,69 @@ import androidx.navigation.Navigation;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import edu.wisc.ece.pinpoint.R;
 import edu.wisc.ece.pinpoint.data.User;
 import edu.wisc.ece.pinpoint.utils.FirebaseDriver;
+import edu.wisc.ece.pinpoint.utils.PlacesAPIDriver;
 import edu.wisc.ece.pinpoint.utils.ValidationUtils;
 
 public class EditProfileFragment extends Fragment {
     private FirebaseDriver firebase;
     private NavController navController;
     private TextInputLayout usernameInputLayout;
+    private TextInputLayout locationInputLayout;
     private EditText usernameInput;
     private EditText locationInput;
     private EditText bioInput;
     private ImageView profilePicUpload;
-    private ActivityResultLauncher<Intent> launcher;
+    private ActivityResultLauncher<Intent> photoPickerLauncher;
+    private ActivityResultLauncher<Intent> locationAutocompleteLauncher;
     private Uri photo;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         firebase = FirebaseDriver.getInstance();
-        // create the launcher that will obtain and upload image
-        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                        photo = result.getData().getData();
-                        Glide.with(requireContext()).load(photo).circleCrop()
-                                .into(profilePicUpload);
-                    }
-                });
+        // Required to initialize places API
+        PlacesAPIDriver.getInstance(requireContext());
+        photoPickerLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+                            if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                                photo = result.getData().getData();
+                                Glide.with(requireContext()).load(photo).circleCrop()
+                                        .into(profilePicUpload);
+                            }
+                        });
+        locationAutocompleteLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+                            int resultCode = result.getResultCode();
+                            Intent data = result.getData();
+                            if (resultCode == RESULT_OK && data != null) {
+                                locationInputLayout.setErrorEnabled(false);
+                                String address = Autocomplete.getPlaceFromIntent(data).getAddress();
+                                if (address != null) {
+                                    address = address.replaceAll("[, ]?\\d{5}", "");
+                                }
+                                locationInput.setText(address);
+                                bioInput.requestFocus();
+                            } else if (resultCode != RESULT_CANCELED) {
+                                locationInputLayout.setError(
+                                        getString(R.string.location_error_text));
+                            }
+                        });
     }
 
     @Override
@@ -69,18 +101,25 @@ public class EditProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
         usernameInputLayout = requireView().findViewById(R.id.profile_edit_username_layout);
+        locationInputLayout = requireView().findViewById(R.id.profile_edit_location_layout);
         usernameInput = requireView().findViewById(R.id.profile_edit_username);
         locationInput = requireView().findViewById(R.id.profile_edit_location);
         bioInput = requireView().findViewById(R.id.profile_edit_bio);
         profilePicUpload = requireView().findViewById(R.id.profile_edit_image);
-
         ImageButton cancelButton = requireView().findViewById(R.id.profile_edit_cancel);
         ImageButton saveButton = requireView().findViewById(R.id.profile_edit_save);
+
+        // Make location input open autocomplete picker instead of allowing typing
+        locationInput.setKeyListener(null);
+        locationInput.setOnFocusChangeListener(this::launchLocationAutocomplete);
+        locationInput.setOnClickListener(
+                (locationView) -> launchLocationAutocomplete(locationView, true));
 
         cancelButton.setOnClickListener(
                 (buttonView) -> navController.navigate(EditProfileFragmentDirections.profile()));
         saveButton.setOnClickListener(this::save);
         profilePicUpload.setOnClickListener(this::uploadPicture);
+
         User cachedUser = firebase.getCachedUser(firebase.getCurrentUser().getUid());
         if (cachedUser != null) {
             usernameInput.setText(cachedUser.getUsername());
@@ -90,71 +129,77 @@ public class EditProfileFragment extends Fragment {
         }
     }
 
-    public void save(View buttonView) {
-        boolean isValid = true;
+    private void save(View buttonView) {
         if (ValidationUtils.isEmpty(usernameInput)) {
             usernameInputLayout.setError(getString(R.string.missing_username));
-            isValid = false;
+            return;
         } else {
             usernameInputLayout.setErrorEnabled(false);
         }
-        // TODO: somehow validate location (implement required autocomplete)
 
-        if (isValid) {
-            buttonView.setEnabled(false);
-            String uid = firebase.getCurrentUser().getUid();
-            User cachedUser = firebase.getCachedUser(uid);
+        buttonView.setEnabled(false);
+        String uid = firebase.getCurrentUser().getUid();
+        User cachedUser = firebase.getCachedUser(uid);
 
-            String oldUsername = cachedUser.getUsername();
-            String oldLocation = cachedUser.getLocation();
-            String oldBio = cachedUser.getBio();
-            String oldProfilePicUrl = cachedUser.getProfilePicUrl();
+        String oldUsername = cachedUser.getUsername();
+        String oldLocation = cachedUser.getLocation();
+        String oldBio = cachedUser.getBio();
+        String oldProfilePicUrl = cachedUser.getProfilePicUrl();
 
-            String newUsername = usernameInput.getText().toString().trim();
-            String newLocation = locationInput.getText().toString().trim().equals("") ? null :
-                    locationInput.getText().toString().trim();
-            String newBio = bioInput.getText().toString().trim().equals("") ? null :
-                    bioInput.getText().toString().trim();
+        String newUsername = usernameInput.getText().toString().trim();
+        String newLocation = locationInput.getText().toString().trim().equals("") ? null :
+                locationInput.getText().toString().trim();
+        String newBio = bioInput.getText().toString().trim().equals("") ? null :
+                bioInput.getText().toString().trim();
 
-            OnCompleteListener<Void> saveUserDataListener = task -> {
+        OnCompleteListener<Void> saveUserDataListener = task -> {
+            if (task.isSuccessful()) {
+                navController.navigate(EditProfileFragmentDirections.profile());
+            } else {
+                cachedUser.setUsername(oldUsername).setLocation(oldLocation).setBio(oldBio)
+                        .setProfilePicUrl(oldProfilePicUrl, false);
+                Toast.makeText(requireContext(), "Couldn't save profile. Try again later.",
+                        Toast.LENGTH_LONG).show();
+                buttonView.setEnabled(true);
+            }
+        };
+
+        if (photo != null) {
+            StorageReference pictureRef =
+                    FirebaseStorage.getInstance().getReference().child("users/" + uid);
+            pictureRef.putFile(photo).addOnCompleteListener((task) -> {
                 if (task.isSuccessful()) {
-                    navController.navigate(EditProfileFragmentDirections.profile());
+                    pictureRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        cachedUser.setUsername(newUsername).setLocation(newLocation).setBio(newBio)
+                                .setProfilePicUrl(uri.toString());
+                        cachedUser.save(uid).addOnCompleteListener(saveUserDataListener);
+                    });
                 } else {
-                    cachedUser.setUsername(oldUsername).setLocation(oldLocation).setBio(oldBio)
-                            .setProfilePicUrl(oldProfilePicUrl, false);
-                    Toast.makeText(requireContext(), "Couldn't save profile. Try again later.",
+                    Toast.makeText(requireActivity(), "Image upload failed. Try again later.",
                             Toast.LENGTH_LONG).show();
                     buttonView.setEnabled(true);
                 }
-            };
-
-            if (photo != null) {
-                StorageReference pictureRef =
-                        FirebaseStorage.getInstance().getReference().child("users/" + uid);
-                pictureRef.putFile(photo).addOnCompleteListener((task) -> {
-                    if (task.isSuccessful()) {
-                        pictureRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                            cachedUser.setUsername(newUsername).setLocation(newLocation)
-                                    .setBio(newBio).setProfilePicUrl(uri.toString());
-                            cachedUser.save(uid).addOnCompleteListener(saveUserDataListener);
-                        });
-                    } else {
-                        Toast.makeText(requireActivity(), "Image upload failed. Try again later.",
-                                Toast.LENGTH_LONG).show();
-                        buttonView.setEnabled(true);
-                    }
-                });
-            } else {
-                cachedUser.setUsername(newUsername).setLocation(newLocation).setBio(newBio);
-                cachedUser.save(uid).addOnCompleteListener(saveUserDataListener);
-            }
+            });
+        } else {
+            cachedUser.setUsername(newUsername).setLocation(newLocation).setBio(newBio);
+            cachedUser.save(uid).addOnCompleteListener(saveUserDataListener);
         }
     }
 
     private void uploadPicture(View view) {
         // launch gallery opening intent
-        launcher.launch(
+        photoPickerLauncher.launch(
                 new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+    }
 
+    private void launchLocationAutocomplete(View view, boolean isFocused) {
+        if (isFocused) {
+            List<String> placesFilter = Arrays.asList("locality", "sublocality", "colloquial_area",
+                    "administrative_area_level_2", "administrative_area_level_3");
+            List<Place.Field> requestedFields = Collections.singletonList(Place.Field.ADDRESS);
+            locationAutocompleteLauncher.launch(
+                    new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,
+                            requestedFields).setTypesFilter(placesFilter).build(requireContext()));
+        }
     }
 }
