bb6f744904027234d723040f0da9e5bc7abbe2a7
Mark <mxj220@gmail.com>
2023-03-28 16:47:03 -0500

    Clickable pin, clickable info box

--- app/src/main/java/edu/wisc/ece/pinpoint/pages/map/MapFragment.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/pages/map/MapFragment.java
@@ -1,22 +1,31 @@
 package edu.wisc.ece.pinpoint.pages.map;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -40,23 +49,23 @@ public class MapFragment extends Fragment {
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
         supportMapFragment.getMapAsync(googleMap -> {
             getDeviceLocation(googleMap);
-            googleMap.setOnMapClickListener(latLng -> {
-                // When clicked on map
-                // Initialize marker options
-                MarkerOptions markerOptions=new MarkerOptions();
-                // Set position of marker
-                markerOptions.position(latLng);
-                // Set title of marker
-                markerOptions.title(latLng.latitude+" : "+latLng.longitude);
-                // Remove all marker
-                googleMap.clear();
-                // Animating to zoom the marker
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
-                // Add marker on map
-                googleMap.addMarker(markerOptions);
+            loadPins(googleMap);
+            googleMap.setOnInfoWindowClickListener(marker -> {
+                Toast.makeText(requireContext(), "Window clicked", Toast.LENGTH_SHORT).show();
             });
         });
         return view;
+    }
+
+    private void loadPins(GoogleMap map){
+        LatLng unionSouth = new LatLng(43.0719999, -89.4098351);
+
+        MarkerOptions uSouthMap = new MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                .alpha(0.5f)
+                .position(unionSouth)
+                .title("Union South Boyyyyyyy");
+        map.addMarker(uSouthMap);
     }
 
     private void getDeviceLocation(GoogleMap map) {
