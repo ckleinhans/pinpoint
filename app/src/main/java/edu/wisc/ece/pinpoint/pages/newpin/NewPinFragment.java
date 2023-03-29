8e02015b569a665c2ece6f8170aef39009f1c65f
Benjamin <petebs10@gmail.com>
2023-03-29 17:22:23 -0500

    remove unncessary import

--- app/src/main/java/edu/wisc/ece/pinpoint/pages/newpin/NewPinFragment.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/pages/newpin/NewPinFragment.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
