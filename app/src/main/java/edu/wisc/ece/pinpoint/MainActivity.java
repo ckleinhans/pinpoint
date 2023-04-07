1570ec95d54ae2b1a935bbaf7e40a774ae41d18a
Sriram Alla <breddy@wisc.edu>
2023-04-07 01:03:04 -0500

    notification

--- app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.wisc.ece.pinpoint;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
@@ -7,6 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,14 +19,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import edu.wisc.ece.pinpoint.utils.NotificationDriver;
+import edu.wisc.ece.pinpoint.utils.PinNotificationActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final List<Integer> hiddenNavbarFragments =
             Arrays.asList(R.id.settings_container_fragment, R.id.edit_profile_fragment,
                     R.id.new_pin_fragment);
     private NavController navController;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,9 +53,24 @@ public class MainActivity extends AppCompatActivity {
                 mapButton.setVisibility(View.VISIBLE);
             }
         });
+
+        PeriodicWorkRequest saveRequest =
+                new PeriodicWorkRequest.Builder(PinNotificationActivity.class, 20, TimeUnit.MINUTES)
+                        // Constraints
+                        .build();
+
+        WorkManager work = WorkManager.getInstance();
+        work.enqueue(saveRequest);
+
+
     }
+
+
+
 
     public void onMapButtonClick(View view) {
         navController.navigate(R.id.navbar_map);
     }
+
+
 }
--- app/src/main/java/edu/wisc/ece/pinpoint/utils/PinNotificationActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/utils/PinNotificationActivity.java
@@ -9,47 +9,36 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.work.ListenableWorker;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
 import edu.wisc.ece.pinpoint.R;
 import edu.wisc.ece.pinpoint.data.OrderedHashSet;
 import edu.wisc.ece.pinpoint.pages.pins.PinListAdapter;
 import edu.wisc.ece.pinpoint.pages.pins.PinListFragment;
 
-public class PinNotificationActivity extends Fragment {
+public class PinNotificationActivity extends Worker {
 
-    Button button;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public PinNotificationActivity(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
     }
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_pin_notification, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        button = view.findViewById(R.id.button2);
-
-
-
+    public Result doWork() {
         NotificationDriver notificationDriver = NotificationDriver.getInstance(null);
-        notificationDriver.sendOneShot("new", "message");
-
-
+        notificationDriver.sendOneShot("pin", "found");
+        return Result.success();
     }
-
-
-
 }
