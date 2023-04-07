35d0a1c770b8a1a1183520d049dfa99a088d4e95
Sriram Alla <breddy@wisc.edu>
2023-04-07 01:03:04 -0500

    notification

--- app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.wisc.ece.pinpoint;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,8 +20,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import edu.wisc.ece.pinpoint.utils.NotificationDriver;
+import edu.wisc.ece.pinpoint.utils.PinNotificationActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
@@ -24,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
             Arrays.asList(R.id.settings_container_fragment, R.id.edit_profile_fragment,
                     R.id.new_pin_fragment);
     private NavController navController;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,28 +56,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        PeriodicWorkRequest saveRequest =
+                new PeriodicWorkRequest.Builder(PinNotificationActivity.class, 20, TimeUnit.MINUTES)
+                        // Constraints
+                        .build();
 
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    PeriodicWorkRequest saveRequest =
-                                            new PeriodicWorkRequest.Builder(PinNotificationActivity.class, 15, TimeUnit.MINUTES)
-                                                    // Constraints
-                                                    .build();
-
-                                    WorkManager work = WorkManager.getInstance(getApplicationContext());
-                                    work.enqueue(saveRequest);
-                                }
-
-
-        },1);
-
-        //setupAlarm();
-
-
-
+        WorkManager work = WorkManager.getInstance();
+        work.enqueue(saveRequest);
 
 
     }
@@ -79,4 +73,6 @@ public class MainActivity extends AppCompatActivity {
     public void onMapButtonClick(View view) {
         navController.navigate(R.id.navbar_map);
     }
+
+
 }
