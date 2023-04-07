24206f291267346c8adda9212038bc1f9cec0ff9
Sriram Alla <breddy@wisc.edu>
2023-04-07 01:03:04 -0500

    notification

--- app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
@@ -66,7 +66,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
+
     public void onMapButtonClick(View view) {
         navController.navigate(R.id.navbar_map);
     }
+
+
 }
