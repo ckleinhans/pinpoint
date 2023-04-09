e88ff651ae468895af1c13103d12eaadb591edd6
Sriram Alla <breddy@wisc.edu>
2023-04-09 18:18:19 -0500

    notifications

--- app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.wisc.ece.pinpoint;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
@@ -7,6 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,15 +19,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import edu.wisc.ece.pinpoint.utils.FirebaseDriver;
 import edu.wisc.ece.pinpoint.utils.NotificationDriver;
+import edu.wisc.ece.pinpoint.utils.PinNotificationActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final List<Integer> hiddenNavbarFragments =
             Arrays.asList(R.id.settings_container_fragment, R.id.edit_profile_fragment,
                     R.id.new_pin_fragment);
     private NavController navController;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    PeriodicWorkRequest saveRequest =
+                                            new PeriodicWorkRequest.Builder(PinNotificationActivity.class, 15, TimeUnit.MINUTES)
+                                                    // Constraints
+                                                    .build();
         // Fetch logged in user profile, followers/following, & activity on app load
         FirebaseDriver firebase = FirebaseDriver.getInstance();
         String uid = firebase.getCurrentUser().getUid();
@@ -58,9 +77,12 @@ public class MainActivity extends AppCompatActivity {
                         // Constraints
                         .build();
 
-        WorkManager work = WorkManager.getInstance();
-        work.enqueue(saveRequest);
+                                    WorkManager work = WorkManager.getInstance(getApplicationContext());
+                                    work.enqueue(saveRequest);
+                                }
 
+
+        },1);
 
     }
 
