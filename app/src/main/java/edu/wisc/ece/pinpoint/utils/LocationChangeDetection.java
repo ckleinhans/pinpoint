90da7c8e91f33989fffc4ff3a6b72b419ccaeeae
Sriram Alla <breddy@wisc.edu>
2023-04-30 18:36:50 -0500

    settings popup

--- app/src/main/java/edu/wisc/ece/pinpoint/utils/LocationChangeDetection.java
+++ app/src/main/java/edu/wisc/ece/pinpoint/utils/LocationChangeDetection.java
@@ -1,11 +1,17 @@
 package edu.wisc.ece.pinpoint.utils;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.preference.PreferenceManager;
@@ -18,6 +24,7 @@ import com.google.android.gms.tasks.Task;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.wisc.ece.pinpoint.R;
 import edu.wisc.ece.pinpoint.data.NearbyPinData;
 import edu.wisc.ece.pinpoint.data.OrderedPinMetadata;
 
@@ -35,6 +42,7 @@ public class LocationChangeDetection extends Worker {
     SharedPreferences preferences;
     NotificationDriver notificationDriver;
     private static final double EARTH_RADIUS_MILES = 3959;
+    int n;
 
 
     public LocationChangeDetection(@NonNull Context context, @NonNull WorkerParameters workerParams) {
@@ -54,35 +62,63 @@ public class LocationChangeDetection extends Worker {
             if (locationDriver.hasCoarseLocation(context)) {
                 locationDriver.getLastLocation(context).addOnCompleteListener(task -> {
 
-                        newLoc = task.getResult();
+                    preferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+                    if(preferences.contains("counter2")) {
+                    }
+                    else{
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+                        builder1.setMessage("Write your message here.");
+                        builder1.setCancelable(true);
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+                        intent.setData(uri);
+                        context.startActivity(intent);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("counter2", String.valueOf(1));
+                        editor.apply();
+                        notificationDriver = NotificationDriver.getInstance(context);
+                        notificationDriver.updatePersistent("Location", "Enable Background Location Access");
+
+                    }
+
+
+                    newLoc = task.getResult();
+
                     if (newLoc != null) {
                         preferences = PreferenceManager.getDefaultSharedPreferences(context);
-                        if (preferences.contains("longitude") && preferences.contains("latitude") ) {
+                        if (preferences.contains("longitude") && preferences.contains("latitude")) {
 
                             double newLat = newLoc.getLatitude();
                             double newLong = newLoc.getLongitude();
-                            double distance = calcDistanceMiles(newLat , Double.parseDouble(preferences.getString("latitude","")) ,
-                                    newLong, Double.parseDouble(preferences.getString("longitude","")));
+                            double distance = calcDistanceMiles(newLat, Double.parseDouble(preferences.getString("latitude", "")),
+                                    newLong, Double.parseDouble(preferences.getString("longitude", "")));
                             Log.d("dist", String.valueOf(distance));
 
                             if (newLoc != null && (distance <= 0.5)) {
+                                notificationDriver = NotificationDriver.getInstance(context);
+                                notificationDriver.updatePersistent("Pins", "too close");
+
+
                             } else {
                                 firebaseDriver = FirebaseDriver.getInstance();
                                 firebaseDriver.fetchNearbyPins(newLoc).addOnCompleteListener(task1 -> {
                                     nearbyPins = task1.getResult();
-                                        int i = nearbyPins.size();
-                                        x = String.valueOf(i);
-                                        notificationDriver = NotificationDriver.getInstance(context);
-                                        notificationDriver.updatePersistent("Pins", x);
-                                        SharedPreferences.Editor editor = preferences.edit();
-                                        editor.putString("longitude", String.valueOf(newLoc.getLongitude()));
-                                        editor.apply();
-                                        editor.putString("latitude", String.valueOf(newLoc.getLatitude()));
-                                        editor.apply();
+                                    int i = nearbyPins.size();
+                                    x = String.valueOf(i);
+                                    notificationDriver = NotificationDriver.getInstance(context);
+                                    notificationDriver.updatePersistent("Pins", x);
+                                    SharedPreferences.Editor editor = preferences.edit();
+                                    editor.putString("longitude", String.valueOf(newLoc.getLongitude()));
+                                    editor.apply();
+                                    editor.putString("latitude", String.valueOf(newLoc.getLatitude()));
+                                    editor.apply();
 
                                 });
                             }
                         } else {
+                            Log.d("loc","new4");
                             preferences.edit().remove("longitude").commit();
                             preferences.edit().remove("latitude").commit();
                             SharedPreferences.Editor editor = preferences.edit();
@@ -94,14 +130,13 @@ public class LocationChangeDetection extends Worker {
 
                     }
                 });
-
-
             }
+
 
 
         }, 1);
 
-        return Result.success();
+        return Result.retry();
     }
 
     public static double calcDistanceMiles(double lat1, double lat2, double long1, double long2) {
